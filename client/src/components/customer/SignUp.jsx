27b59c16b6de38c1f88f1c1bb60a5e412b461847
Sanchit <uppalsanchit9@gmail.com>
2023-08-11 12:43:52 +0530

    Done

--- client/src/components/customer/SignUp.jsx
+++ client/src/components/customer/SignUp.jsx
@@ -85,7 +85,7 @@ export default function SignUp() {
           <LockIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Business's Sign up
+          Customer Sign up
         </Typography>
         <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
           <Grid container spacing={2}>
